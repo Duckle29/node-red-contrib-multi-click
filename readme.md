56339e7af2935fcaed4614d7f82bf8f7a4f9bac9
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2022-01-02 00:28:40 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Multi click node
+
+This is a node that takes a button `down` or `up` event, and adds more functionality
+to them like double click, press and hold and release events.
+
+## Usage
+Enter the string used for down events and up events, and the timeout[ms] to wait for a double-press.
+
+Keep in mind that the higher the timeout, the more latency will be added to your buttons
---
+++ src/multi-click.js
@@ -0,0 +1,82 @@
+module.exports = function(RED) {
+    function multiClick(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.triggerProperty = config.triggerProperty;
+        node.eventDown = config.eventDown;
+        node.eventUp = config.eventUp;
+        node.timeout = config.timeout;
+
+        // idle -> down1 -> up -> timeout = single press
+        // idle -> down1 -> timeout = held
+            // held -> up  = released
+        // idle -> down1 -> up -> down2 -> up = double-press
+        node.state = "idle"
+        node.last_event = 0;
+        function stateMachine(msg, send, done) 
+        {
+            console.log(msg[node.triggerProperty]);
+            console.log(" state:" + node.state);
+
+            console.log(" !idle:" + (node.state != "idle"));
+            
+            if(msg.timeout)
+            {
+                console.log(" " + 1);
+                switch(node.state)
+                {
+                    case "up":
+                        node.send([{payload: "single"},,,]);
+                        node.state = "idle";
+                        break;
+                    case "down1":
+                        node.send([,,{payload: "held"},]);
+                        node.state = "held";
+                        break;
+                    default:
+                        node.state = "idle";
+                        break;
+                }
+            }
+            else if (msg[node.triggerProperty] == node.eventDown)
+            {
+                console.log(" " + 2);
+                switch(node.state)
+                {
+                    case "idle":
+                        node.state = "down1";
+                        setTimeout(function() {
+                            msg.timeout = true;
+                            stateMachine(msg, send, done);
+                        }, node.timeout);
+                        break;
+                    case "up":
+                        node.send([,{payload: "double"},,]);
+                        node.state = "idle";
+                        break;
+                }
+            }
+            else if (msg[node.triggerProperty] == node.eventUp)
+            {
+                console.log(" " + 3);
+                switch(node.state)
+                {
+                    case "down1":
+                        node.state = "up";
+                        setTimeout(function() {
+                            msg.timeout = true;
+                            stateMachine(msg, send, done);
+                        }, node.timeout);
+                        break;
+                    case "held":
+                        node.send([,,,{payload: "released"}]);
+                        node.state = "idle";
+                        break;
+                }
+            }
+        }
+
+        node.on('input', stateMachine);
+    }
+    RED.nodes.registerType("multi click", multiClick);
+}
